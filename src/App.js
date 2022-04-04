ac448ebf89d4b1f61adb10e4c5a18ad9bd482d8d
Riley Rochester <rochester.r@northeastern.edu>
2022-04-04 19:36:14 -0400

    fix nav for netlify

--- src/App.js
+++ src/App.js
@@ -19,8 +19,9 @@ function App() {
             <div className="container">
                 <Routes>
                     <Route path="/" >
+                        <Route index element={<HelloWorld />} />
                         <Route path="labs" element={<Labs />} />
-                        <Route path="hello" element={<HelloWorld />} />
+                        <Route path="hello" exact={true} element={<HelloWorld />} />
                         <Route path="tuiter" element={<Tuiter />} >
                             <Route index element={<HomeScreen />} />
                             <Route path="explore" element={<ExploreScreen />} />
